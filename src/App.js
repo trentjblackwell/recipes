872fb59984700a7bfb32150f8f50a35b714c672b
TrentB <trentjblackwell@gmail.com>
2019-08-29 20:14:27 -0700

    initial setup, API keys + search form

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '15fdf7f3';
+  const APP_KEY = 'c140fc867e7611b9ff2d3739de62260d';
+
+  const exampleReq =
+    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form className="search-form">
+       <input className="search-bar" type="text"/>
+       <button className="search=btn" type="submit">Search</button>
+     </form>
     </div>
   );
-}
+};
 
 export default App;
