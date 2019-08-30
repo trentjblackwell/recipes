18807cdd99eae75850b07bdd608f00d0be1798b3
TrentB <trentjblackwell@gmail.com>
2019-08-29 20:51:45 -0700

    pulling + rendering specified data from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import Recipe from './recipe';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = '15fdf7f3';
   const APP_KEY = 'c140fc867e7611b9ff2d3739de62260d';
-  // const exampleReq =
-  //   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+ 
+  const [recipes, setRecipes] = useState([]);
 
     useEffect( () => {
       getRecipes();
@@ -15,7 +16,8 @@ const App = () => {
     const getRecipes = async () => {
       const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
       const data = await response.json();
-      console.log(data);
+      setRecipes(data.hits);
+      console.log(data.hits);
     }
 
   return (
@@ -24,6 +26,13 @@ const App = () => {
        <input className="search-bar" type="text"/>
        <button className="search=btn" type="submit"> Search </button>
     </form>
+    {recipes.map(recipe => (
+      <Recipe 
+      title={recipe.recipe.label} 
+      calories={recipe.recipe.calories}
+      image={recipe.recipe.image}
+      />
+    ))};
     </div>
   );
 };
